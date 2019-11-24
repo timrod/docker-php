ba69f65a63526e57c270636f56573a9ac7d93f90
timrod <timrod.v@ya.ru>
2019-11-24 13:55:32 +0400

    add chown

--- 5.6/docker-init.sh
+++ 5.6/docker-init.sh
@@ -5,6 +5,8 @@ useradd -g $APACHE_RUN_USER -u $APACHE_RUN_USER_ID $APACHE_RUN_USER
 
 set -e
 
+chown -R ${APACHE_RUN_USER}:${APACHE_RUN_USER_ID} ${WWW_PATH}
+
 if [[ $RSYNC_DAEMON == 'true' ]]; then
   nohup rsync-daemon.sh &
 fi
